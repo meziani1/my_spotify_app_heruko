1acd311146afc6aa4bcb27accee09ee28866ae67
meziani1 <85911812+meziani1@users.noreply.github.com>
2022-09-23 16:26:20 +0000

    Delete DeployML directory

--- DeployML/DjangoAPI/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers 
-from .models import Skipped
-
-class SkippedSerializers(serializers.ModelSerializer): 
-    class meta: 
-        model=Skipped 
-        fields='__all__'
